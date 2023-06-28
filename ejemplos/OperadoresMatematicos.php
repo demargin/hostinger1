e3e6b58b3b2e82fa42214e11977f9e08dc286086
demargin <60243086+demargin@users.noreply.github.com>
2023-06-28 12:24:41 -0500

    Operadores matematicos

---
+++ ejemplos/OperadoresMatematicos.php
@@ -0,0 +1,29 @@
+<?php
+    $a = 5;
+    $b = 3;
+    $c = "7";
+
+    $suma = $a + $b;
+    $resta = $a - $b;
+    $multi = $a * $b;
+    $poten = $a ++ $b;
+    $divi = $a / $b;
+    $modulo = $a % $b ;
+    $ident = +$c;
+    $negac = -$a;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <a href="index.php">Volver a la pagina INICIO</a>
+    <title>Operadores Matematicos</title>
+
+
+</head>
+<body>
+    
+</body>
+</html>
