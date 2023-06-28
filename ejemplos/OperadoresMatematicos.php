2d010e31980de58f5441611a4bd9e492f2dec681
demargin <60243086+demargin@users.noreply.github.com>
2023-06-28 13:43:28 -0500

    opoeradoresmatematicos 1ra modificacion

--- ejemplos/OperadoresMatematicos.php
+++ ejemplos/OperadoresMatematicos.php
@@ -20,10 +20,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <a href="index.php">Volver a la pagina INICIO</a>
     <title>Operadores Matematicos</title>
-
-
 </head>
 <body>
-    
+    <p>
+        para ejemplificar los operadores en PHP usaremos los siguuiente valores:
+            a = 5 
+            b = 3
+            c = "7"
+    </p>
+    <h3>la suma de a + b = <?=$suma?></h3><br>
+    <h3>la resta de a - b = <?=$resta?></h3><br>
+    <h3>la multiplicacion de a x b = <?=$multi?></h3><br>
+    <h3>la Potencia de a elevado a la b = <?=$poten?></h3><br>
+    <h3>la Divicion de a / b = <?=$divi?></h3><br>
+    <h3>la El residuo de dividir o el resto de a / b = <?=$modulo?></h3><br>
+    <h3>la La identidad (convetir una variable string a int) de c es = <?=$ident?></h3><br>
+    <h3>la la negacion del valor de "a" es :: <?=$negac?></h3><br>
 </body>
 </html>
