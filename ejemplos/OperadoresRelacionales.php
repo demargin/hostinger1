2e7bbb95a310c8ff2ad6d04140cbad374e69e059
demargin <60243086+demargin@users.noreply.github.com>
2023-06-28 18:52:36 -0500

    operadores lineales

---
+++ ejemplos/OperadoresRelacionales.php
@@ -0,0 +1,35 @@
+<?php
+    $a = 5;
+    $b = 3;
+    $c = "5";
+    $d = 11; 
+
+    echo ($valor1 == $valor2) ? "Los valores son iguales" : "Los valores son diferentes";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <a href="index.php">Volver a la pagina INICIO</a>
+    <title>Operadores Matematicos</title>
+</head>
+<body>
+    <h2>
+        Para ejemplificar los operadores en PHP usaremos los siguiente valores:
+
+        </h2>
+        <h3> ----> a = 5</h3>
+        <h3> ----> b = 3</h3>
+        <h3> ----> c = "7"</h3>
+    <h3>la suma de a + b = <?=$suma?></h3>
+    <h3>la resta de a - b = <?=$resta?></h3>
+    <h3>la multiplicacion de a x b = <?=$multi?></h3>
+    <h3>la Potencia de a elevado a la b = <?=$poten?></h3>
+    <h3>la Division de a / b = <?=$divi?></h3>
+    <h3>la El residuo de dividir o el resto de a / b = <?=$modulo?></h3>
+    <h3>la identidad (convetir una variable string a int) de c es = <?=$ident?></h3>
+    <h3>la negacion del valor de "a" es :: <?=$negac?></h3>
+</body>
+</html>
