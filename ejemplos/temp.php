0414425ac76af548eb080ad88a13ee5162c73acb
demargin <60243086+demargin@users.noreply.github.com>
2023-07-06 17:41:45 -0500

    sadasd

--- ejemplos/temp.php
+++ ejemplos/temp.php
@@ -5,36 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Temporal para pruebas</title>
 
-    <script>
-  var radioButtons = document.querySelectorAll('input[type="radio"]');
-  var maxSelections = 2;
-  var numSelections = 0;
-
-  radioButtons.forEach(function(radioButton) {
-    radioButton.addEventListener('click', function() {
-      var selectedRadios = document.querySelectorAll('input[type="radio"]:checked');
-      numSelections = selectedRadios.length;
-
-      if (numSelections > maxSelections) {
-        this.checked = false;
-      }
+  <script>
+    // Coloca aquí el código JavaScript
+    var radioButtons = document.querySelectorAll('input[type="radio"]');
+    var maxSelections = 2;
+    
+    radioButtons.forEach(function(radioButton) {
+      radioButton.addEventListener('click', function() {
+        var selectedRadios = document.querySelectorAll('input[type="radio"]:checked');
+        
+        if (selectedRadios.length > maxSelections) {
+          this.checked = false;
+        }
+      });
     });
-  });
-</script>
+  </script>
 </head>
 <body>
-
-<form action="procesar.php" method="GET">
-  <input type="radio" name="opcion" value="opcion1"> Opción 1<br>
-  <input type="radio" name="opcion" value="opcion2"> Opción 2<br>
-  <input type="radio" name="opcion" value="opcion3"> Opción 3<br>
-  <input type="radio" name="opcion" value="opcion4"> Opción 4<br>
-  <input type="radio" name="opcion" value="opcion5"> Opción 5<br>
-  
-  <input type="submit" value="Enviar">
-</form>
-
-
-
+  <!-- Contenido de tu página -->
+  <form action="procesar.php" method="GET">
+    <input type="radio" name="opcion[]" value="opcion1"> Opción 1<br>
+    <input type="radio" name="opcion[]" value="opcion2"> Opción 2<br>
+    <input type="radio" name="opcion[]" value="opcion3"> Opción 3<br>
+    <input type="radio" name="opcion[]" value="opcion4"> Opción 4<br>
+    <input type="radio" name="opcion[]" value="opcion5"> Opción 5<br>
+    
+    <input type="submit" value="Enviar">
+  </form>
 </body>
-</html>
+</html
