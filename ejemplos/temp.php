0475f232b2debe598772bf5c0e19530942600db5
demargin <60243086+demargin@users.noreply.github.com>
2023-07-06 17:39:51 -0500

    ordenado

--- ejemplos/temp.php
+++ ejemplos/temp.php
@@ -4,20 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Temporal para pruebas</title>
-</head>
-<body>
 
-<form action="procesar.php" method="GET">
-  <input type="radio" name="opcion" value="opcion1"> Opción 1<br>
-  <input type="radio" name="opcion" value="opcion2"> Opción 2<br>
-  <input type="radio" name="opcion" value="opcion3"> Opción 3<br>
-  <input type="radio" name="opcion" value="opcion4"> Opción 4<br>
-  <input type="radio" name="opcion" value="opcion5"> Opción 5<br>
-  
-  <input type="submit" value="Enviar">
-</form>
-
-<script>
+    <script>
   var radioButtons = document.querySelectorAll('input[type="radio"]');
   var maxSelections = 2;
   var numSelections = 0;
@@ -33,6 +21,20 @@
     });
   });
 </script>
+</head>
+<body>
+
+<form action="procesar.php" method="GET">
+  <input type="radio" name="opcion" value="opcion1"> Opción 1<br>
+  <input type="radio" name="opcion" value="opcion2"> Opción 2<br>
+  <input type="radio" name="opcion" value="opcion3"> Opción 3<br>
+  <input type="radio" name="opcion" value="opcion4"> Opción 4<br>
+  <input type="radio" name="opcion" value="opcion5"> Opción 5<br>
+  
+  <input type="submit" value="Enviar">
+</form>
+
+
 
 </body>
 </html>
