ecfbc4319c5b352e7741457fe11f10d80637d8e1
demargin <60243086+demargin@users.noreply.github.com>
2023-07-07 02:04:32 -0500

    nuevos cambiso

--- ejemplos/temp.php
+++ ejemplos/temp.php
@@ -1,50 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Ejemplo de HTML, JavaScript y PHP con restricción de dos checkboxes</title>
-    <style>
-        .checkbox-container {
-            margin-bottom: 10px;
-        }
-    </style>
+  <title>Opciones Checkbox</title>
+  <script>
+    // Código JavaScript aquí
+    window.onload = function() {
+      var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+      checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', function() {
+          var checkedCount = document.querySelectorAll('input[type="checkbox"]:checked').length;
+
+          if (checkedCount > 2) {
+            checkboxes.forEach(function(checkbox) {
+              if (!checkbox.checked) {
+                checkbox.disabled = true;
+              }
+            });
+          } else {
+            checkboxes.forEach(function(checkbox) {
+              checkbox.disabled = false;
+            });
+          }
+        });
+      });
+    }
+  </script>
 </head>
 <body>
-
-<h1>Ejemplo de HTML, JavaScript y PHP con restricción de dos checkboxes</h1>
-
-<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <div class="checkbox-container">
-        <input type="checkbox" name="opcion[]" value="Opción 1"> Opción 1
-    </div>
-    <div class="checkbox-container">
-        <input type="checkbox" name="opcion[]" value="Opción 2"> Opción 2
-    </div>
-    <div class="checkbox-container">
-        <input type="checkbox" name="opcion[]" value="Opción 3"> Opción 3
-    </div>
-    <br>
-    <input type="submit" name="submit" value="Enviar">
-</form>
-
-<?php
-if (isset($_POST['submit'])) {
-    if (isset($_POST['opcion'])) {
-        $opcionesSeleccionadas = $_POST['opcion'];
-        $cantidadSeleccionada = count($opcionesSeleccionadas);
-
-        if ($cantidadSeleccionada == 2) {
-            echo "<h2>Opciones seleccionadas:</h2>";
-            foreach ($opcionesSeleccionadas as $opcion) {
-                echo $opcion . "<br>";
-            }
-        } else {
-            echo "<h2>Debe seleccionar exactamente dos opciones.</h2>";
-        }
-    } else {
-        echo "<h2>No se ha seleccionado ninguna opción.</h2>";
-    }
-}
-?>
-
+  <form>
+    <input type="checkbox" name="option" value="option1"> Opción 1<br>
+    <input type="checkbox" name="option" value="option2"> Opción 2<br>
+    <input type="checkbox" name="option" value="option3"> Opción 3<br>
+    <input type="checkbox" name="option" value="option4"> Opción 4<br>
+    <input type="checkbox" name="option" value="option5"> Opción 5<br>
+  </form>
 </body>
 </html>
