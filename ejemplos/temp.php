558bd10b170ee24f323bfc7699d7ab86ea049135
demargin <60243086+demargin@users.noreply.github.com>
2023-07-07 01:58:23 -0500

    camb

--- ejemplos/temp.php
+++ ejemplos/temp.php
@@ -1,26 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Ejemplo de HTML, JavaScript y PHP con checkboxes</title>
+    <title>Ejemplo de HTML, JavaScript y PHP con restricción de dos checkboxes</title>
+    <style>
+        .checkbox-container {
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
 <body>
 
-<h1>Ejemplo de HTML, JavaScript y PHP con checkboxes</h1>
+<h1>Ejemplo de HTML, JavaScript y PHP con restricción de dos checkboxes</h1>
 
 <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="checkbox" name="opcion[]" value="Opción 1"> Opción 1<br>
-    <input type="checkbox" name="opcion[]" value="Opción 2"> Opción 2<br>
-    <input type="checkbox" name="opcion[]" value="Opción 3"> Opción 3<br>
+    <div class="checkbox-container">
+        <input type="checkbox" name="opcion[]" value="Opción 1"> Opción 1
+    </div>
+    <div class="checkbox-container">
+        <input type="checkbox" name="opcion[]" value="Opción 2"> Opción 2
+    </div>
+    <div class="checkbox-container">
+        <input type="checkbox" name="opcion[]" value="Opción 3"> Opción 3
+    </div>
     <br>
     <input type="submit" name="submit" value="Enviar">
 </form>
 
 <?php
 if (isset($_POST['submit'])) {
-    if (!empty($_POST['opcion'])) {
-        echo "<h2>Opciones seleccionadas:</h2>";
-        foreach ($_POST['opcion'] as $opcion) {
-            echo $opcion . "<br>";
+    if (isset($_POST['opcion'])) {
+        $opcionesSeleccionadas = $_POST['opcion'];
+        $cantidadSeleccionada = count($opcionesSeleccionadas);
+
+        if ($cantidadSeleccionada == 2) {
+            echo "<h2>Opciones seleccionadas:</h2>";
+            foreach ($opcionesSeleccionadas as $opcion) {
+                echo $opcion . "<br>";
+            }
+        } else {
+            echo "<h2>Debe seleccionar exactamente dos opciones.</h2>";
         }
     } else {
         echo "<h2>No se ha seleccionado ninguna opción.</h2>";
