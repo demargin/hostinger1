b0f747946a1e816960aac96965dbf5df2af8a45d
demargin <60243086+demargin@users.noreply.github.com>
2023-08-22 04:27:17 -0500

    Index actualizado y ordenado

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
+$thelist = ""; // Inicializar la variable $thelist
+
 if ($handle = opendir('.')) {
-    while (false !== ($file = readdir($handle)))
-    {
-        if (($file != ".") 
-         && ($file != ".."))
-        {
+    while (false !== ($file = readdir($handle))) {
+        if (($file != ".") && ($file != "..")) {
             $thelist .= '<LI><a href="'.$file.'">'.$file.'</a>';
         }
     }
